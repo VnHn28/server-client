f5ecfc5305661262db84a1ae1e6aedd2db3c3b92
Vincent Handoyo <vincenthan28@gmail.com>
2025-09-09 04:36:03 +0800

    update: implement TCP authentication and ACK loop, protocol message file restructuring

--- client/client.go
+++ client/client.go
@@ -1,6 +1,13 @@
 package client
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"server-client/protocol"
+	"time"
+)
 
 type Client struct {
 	Protocol string // "tcp", "udp-unicast", "udp-multicast"
@@ -11,7 +18,35 @@ type Client struct {
 
 // Connect establishes a connection to the server (temp)
 func (c *Client) Connect() error {
-	fmt.Printf("[%s client] Connecting to %s\n", c.Protocol, c.Addr)
+	if c.Protocol == "tcp" {
+		return c.connectTCP()
+	}
+	return fmt.Errorf("protocol %s not implemented yet", c.Protocol)
+}
+
+func (c *Client) connectTCP() error {
+	conn, err := net.Dial("tcp", c.Addr)
+	if err != nil {
+		return fmt.Errorf("tcp dial failed: %w", err)
+	}
+	fmt.Printf("[%s client] connected to %s\n", c.Protocol, c.Addr)
+
+	auth := protocol.AuthMessage{Username: c.Username, Password: c.Password}
+	authBytes, _ := json.Marshal(auth)
+	authBytes = append(authBytes, '\n')
+	_, _ = conn.Write(authBytes)
+
+	tmsg := protocol.TimeMessage{Timestamp: time.Now()}
+	tBytes, _ := json.Marshal(tmsg)
+	tBytes = append(tBytes, '\n')
+	_, _ = conn.Write(tBytes)
+
+	reader := bufio.NewReader(conn)
+	ackBytes, _ := reader.ReadBytes('\n')
+	var ack protocol.AckMessage
+	_ = json.Unmarshal(ackBytes, &ack)
+	fmt.Println("[CLIENT] received ack:", ack.Status)
+
 	return nil
 }
 
--- server/server.go
+++ server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"net"
+	"server-client/protocol"
 	"sync"
 )
 
-type Server struct{}
+type Server struct {
+	mu sync.Mutex
+}
 
 var (
 	instance *Server
@@ -20,10 +26,71 @@ func GetServer() *Server {
 	return instance
 }
 
-// StartTCP starts the TCP listener (temp)
+// StartTCP starts the TCP listener and handles incoming connections
 func (s *Server) StartTCP(addr string) error {
-	fmt.Println("Starting TCP server on", addr)
-	return nil
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("TCP listen error: %w", err)
+	}
+	fmt.Println("[SERVER] TCP listening on", addr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("[SERVER] accept error:", err)
+			continue
+		}
+		go s.handleTCPConn(conn)
+	}
+}
+
+func (s *Server) handleTCPConn(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	authBytes, err := reader.ReadBytes('\n')
+	if err != nil {
+		fmt.Println("[SERVER] failed to read auth:", err)
+		return
+	}
+
+	var auth protocol.AuthMessage
+	if err := json.Unmarshal(authBytes, &auth); err != nil {
+		fmt.Println("[SERVER] invalid auth json:", err)
+		return
+	}
+
+	if !s.validateUser(auth.Username, auth.Password) {
+		fmt.Println("[SERVER] invalid credentials for", auth.Username)
+		return
+	}
+	fmt.Println("[SERVER] client authenticated:", auth.Username)
+
+	for {
+		timeBytes, err := reader.ReadBytes('\n')
+		if err != nil {
+			fmt.Println("[SERVER] connection closed:", err)
+			return
+		}
+
+		var tmsg protocol.TimeMessage
+		if err := json.Unmarshal(timeBytes, &tmsg); err != nil {
+			fmt.Println("[SERVER] invalid time message:", err)
+			continue
+		}
+
+		fmt.Printf("[SERVER] received time from %s: %v\n", auth.Username, tmsg.Timestamp)
+
+		// send ACK
+		ack := protocol.AckMessage{Status: "OK"}
+		ackBytes, _ := json.Marshal(ack)
+		ackBytes = append(ackBytes, '\n')
+		if _, err := conn.Write(ackBytes); err != nil {
+			fmt.Println("[SERVER] failed to send ACK:", err)
+			return
+		}
+	}
 }
 
 // StartUDPUnicast starts the UDP unicast listener (temp)
@@ -36,4 +103,8 @@ func (s *Server) StartUDPUnicast(addr string) error {
 func (s *Server) StartUDPMulticast(addr string) error {
 	fmt.Println("Starting UDP multicast server on", addr)
 	return nil
+}
+
+func (s *Server) validateUser(username, password string) bool {
+	return username != "" && password != ""
 }
