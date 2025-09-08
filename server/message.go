d70c5cbf69691d097c1303a6f458668c3f02aa79
Vincent Handoyo <vincenthan28@gmail.com>
2025-09-09 03:25:05 +0800

    update: add message protocol structs

---
+++ server/message.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"time"
+)
+
+type AuthMessage struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type TimeMessage struct {
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type AckMessage struct {
+	Status string `json:"status"`
+}
