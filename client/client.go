c09108915bdcd2f901f32e35b0832c6f9dc88651
Vincent Handoyo <vincenthan28@gmail.com>
2025-09-09 03:49:14 +0800

    update: add base client struct with Connect, SendAuth, SendTime stubs

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package client
+
+import "fmt"
+
+type Client struct {
+	Protocol string // "tcp", "udp-unicast", "udp-multicast"
+	Addr     string
+	Username string
+	Password string
+}
+
+// Connect establishes a connection to the server (temp)
+func (c *Client) Connect() error {
+	fmt.Printf("[%s client] Connecting to %s\n", c.Protocol, c.Addr)
+	return nil
+}
+
+// SendAuth sends authentication message (username + password) (temp)
+func (c *Client) SendAuth() error {
+	fmt.Printf("[%s client] Sending auth (user=%s)\n", c.Protocol, c.Username)
+	return nil
+}
+
+// SendTime sends current time to server (temp)
+func (c *Client) SendTime() error {
+	fmt.Printf("[%s client] Sending time message\n", c.Protocol)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "server-client/server"
+import (
+	"server-client/client"
+	"server-client/server"
+)
 
 func main() {
+	// Start server
 	srv := server.GetServer()
-
 	go srv.StartTCP(":9000")
 	go srv.StartUDPUnicast(":9001")
 	go srv.StartUDPMulticast(":9002")
+
+	// Start sample client
+	clientA := client.Client{
+		Protocol: "tcp",
+		Addr:     "localhost:9000",
+		Username: "alice",
+		Password: "secret",
+	}
+
+	clientA.Connect()
+	clientA.SendAuth()
+	clientA.SendTime()
 
 	select {}
 }
