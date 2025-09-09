7e0092eed97fe1819c2a972284a78ca53ac0aedd
Vincent Handoyo <vincenthan28@gmail.com>
2025-09-10 04:47:17 +0800

    update: refactor main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"server-client/client"
 	"server-client/server"
 	"time"
@@ -9,79 +10,46 @@ import (
 func main() {
 	// Start server
 	srv := server.GetServer()
-	go srv.StartTCP(":9000")
-	go srv.StartUDPUnicast(":9001")
-	go srv.StartUDPMulticast("224.0.0.1:9002")
-
-	// Client A: TCP
-	clientA := client.Client{
-		Protocol: "tcp",
-		Addr:     "localhost:9000",
-		Username: "alice",
-		Password: "secret",
-	}
-	if err := clientA.Connect(); err != nil {
-		panic(err)
-	}
-
-	// Client B: UDP unicast
-	clientB := client.Client{
-		Protocol: "udp-unicast",
-		Addr:     "localhost:9001",
-		Username: "bob",
-		Password: "secret",
-	}
-	if err := clientB.Connect(); err != nil {
-		panic(err)
-	}
-
-	// Client C: UDP multicast
-	clientC := client.Client{
-		Protocol: "udp-multicast",
-		Addr:     "224.0.0.1:9002",
-		Username: "chris",
-		Password: "secret",
-	}
-	if err := clientC.Connect(); err != nil {
-		panic(err)
-	}
-
-	// Client D: UDP multicast
-	clientD := client.Client{
-		Protocol: "udp-multicast",
-		Addr:     "224.0.0.1:9002",
-		Username: "dick",
-		Password: "secret",
-	}
-	if err := clientD.Connect(); err != nil {
-		panic(err)
-	}
-
-	// Run them all in background loops
 	go func() {
-		for {
-			clientA.SendTime()
-			time.Sleep(7 * time.Second)
+		if err := srv.StartTCP(":9000"); err != nil {
+			fmt.Println("TCP server error:", err)
 		}
 	}()
 	go func() {
-		for {
-			clientB.SendTime()
-			time.Sleep(7 * time.Second)
+		if err := srv.StartUDPUnicast(":9001"); err != nil {
+			fmt.Println("UDP unicast server error:", err)
 		}
 	}()
 	go func() {
-		for {
-			clientC.SendTime()
-			time.Sleep(7 * time.Second)
+		if err := srv.StartUDPMulticast("224.0.0.1:9002"); err != nil {
+			fmt.Println("UDP multicast server error:", err)
 		}
 	}()
-	go func() {
-		for {
-			clientD.SendTime()
-			time.Sleep(7 * time.Second)
+
+	time.Sleep(500 * time.Millisecond)
+
+	// --- Clients ---
+	clients := []*client.Client{
+		{Protocol: "tcp", Addr: "localhost:9000", Username: "alice", Password: "secret"},
+		{Protocol: "udp-unicast", Addr: "localhost:9001", Username: "bob", Password: "secret"},
+		{Protocol: "udp-multicast", Addr: "224.0.0.1:9002", Username: "chris", Password: "secret"},
+		{Protocol: "udp-multicast", Addr: "224.0.0.1:9002", Username: "dick", Password: "secret"},
+	}
+
+	for _, c := range clients {
+		c := c
+		if err := c.Connect(); err != nil {
+			fmt.Printf("[%s client] failed to connect: %v\n", c.Protocol, err)
+			continue
 		}
-	}()
+
+		go func(cli *client.Client) {
+			for {
+				cli.SendTime()
+				time.Sleep(7 * time.Second)
+			}
+		}(c)
+	}
 
 	select {}
 }
